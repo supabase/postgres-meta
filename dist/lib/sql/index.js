e2cf3fb348aea9ff476553faf14115197d7ef22b
Paul Copplestone <pcopplestone@gmail.com>
2020-05-22 10:50:55 +0800

    bundles the whole thing up as a binary

--- dist/lib/sql/index.js
+++ dist/lib/sql/index.js
@@ -2,43 +2,45 @@
 
 var fs = require('fs');
 
+var path = require('path');
+
 module.exports = {
   columns: {
-    list: fs.readFileSync(__dirname + '/columns/list.sql').toString()
+    list: fs.readFileSync(path.join(__dirname, '/columns/list.sql')).toString()
   },
   config: {
-    list: fs.readFileSync(__dirname + '/config/list.sql').toString(),
-    version: fs.readFileSync(__dirname + '/config/version.sql').toString()
+    list: fs.readFileSync(path.join(__dirname, '/config/list.sql')).toString(),
+    version: fs.readFileSync(path.join(__dirname, '/config/version.sql')).toString()
   },
   constraints: {
-    list: fs.readFileSync(__dirname + '/constraints/list.sql').toString()
+    list: fs.readFileSync(path.join(__dirname, '/constraints/list.sql')).toString()
   },
   joins: {
-    list: fs.readFileSync(__dirname + '/joins/list.sql').toString()
+    list: fs.readFileSync(path.join(__dirname, '/joins/list.sql')).toString()
   },
   plugins: {
-    list: fs.readFileSync(__dirname + '/plugins/list.sql').toString()
+    list: fs.readFileSync(path.join(__dirname, '/plugins/list.sql')).toString()
   },
   policies: {
-    list: fs.readFileSync(__dirname + '/policies/list.sql').toString()
+    list: fs.readFileSync(path.join(__dirname, '/policies/list.sql')).toString()
   },
   stats: {
-    tableSize: fs.readFileSync(__dirname + '/stats/table_size.sql').toString()
+    tableSize: fs.readFileSync(path.join(__dirname, '/stats/table_size.sql')).toString()
   },
   sequences: {
-    list: fs.readFileSync(__dirname + '/sequences/list.sql').toString()
+    list: fs.readFileSync(path.join(__dirname, '/sequences/list.sql')).toString()
   },
   types: {
-    list: fs.readFileSync(__dirname + '/types/list.sql').toString()
+    list: fs.readFileSync(path.join(__dirname, '/types/list.sql')).toString()
   },
   tables: {
-    list: fs.readFileSync(__dirname + '/tables/list.sql').toString(),
-    grants: fs.readFileSync(__dirname + '/tables/grants.sql').toString()
+    list: fs.readFileSync(path.join(__dirname, '/tables/list.sql')).toString(),
+    grants: fs.readFileSync(path.join(__dirname, '/tables/grants.sql')).toString()
   },
   users: {
-    list: fs.readFileSync(__dirname + '/users/list.sql').toString()
+    list: fs.readFileSync(path.join(__dirname, '/users/list.sql')).toString()
   },
   views: {
-    list: fs.readFileSync(__dirname + '/views/list.sql').toString()
+    list: fs.readFileSync(path.join(__dirname, '/views/list.sql')).toString()
   }
 };
