22476f8fd73555b86c647a8e6db6fd4b5efb208e
Paul Copplestone <pcopplestone@gmail.com>
2020-06-05 17:45:01 +0800

    Moves all the API calls into the correct URLs

--- dist/lib/sql/users/list.sql
+++ dist/lib/sql/users/list.sql
@@ -1,13 +1,12 @@
 select
-    usename,
-    usesysid,
-    usecreatedb,
-    usesuper,
-    userepl,
-    usebypassrls,
-    passwd,
-    valuntil,
-    useconfig,
+    usename as "name",
+    usesysid as "id",
+    usecreatedb as "has_create_db_privileges",
+    usesuper as "is_super_user",
+    userepl as "has_replication_privileges",
+    usebypassrls as "can_bypass_rls",
+    valuntil as "valid_until", -- Password expiry time (only used for password authentication)
+    useconfig as "user_config", -- Session defaults for run-time configuration variables
     active_connections.connections,
     pg_roles.rolconnlimit as max_user_connections,
     max_db_connections.max_connections as max_db_connections
