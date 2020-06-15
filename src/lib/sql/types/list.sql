5324d0a6760bf3465d2f7c2a784dbd3263f8e2e9
Paul Copplestone <pcopplestone@gmail.com>
2020-06-15 21:49:50 +0800

    refactor: Turns the enums into a JSON array (rather than a PG array)

--- src/lib/sql/types/list.sql
+++ src/lib/sql/types/list.sql
@@ -4,12 +4,12 @@ t.typname  AS name,
 n.nspname as schema,
 pg_catalog.Format_type ( t.oid, NULL ) AS format,
 pg_catalog.obj_description ( t.oid, 'pg_type' ) AS description,
-array (
+array_to_json(array (
 	select   e.enumlabel
 	FROM     pg_catalog.pg_enum e
 	WHERE    e.enumtypid = t.oid
 	ORDER BY e.oid 
-) AS enums
+)) AS enums
 FROM pg_catalog.pg_type t LEFT JOIN pg_catalog.pg_namespace n
 ON n.oid = t.typnamespace
 WHERE (
