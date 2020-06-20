7034a3d457705bd085690e16714b603b59450575
Bobbie Soedirgo <bobbie@soedirgo.dev>
2020-06-21 00:54:41 +0800

    refactor: fix TS not recognizing files as modules
    There was no top level import/export in these files, so TS wasn't able
    to infer that these are modules and spits out `RunQuery` redeclare errors.

--- src/api/query.ts
+++ src/api/query.ts
@@ -1,5 +1,5 @@
-const { Router } = require('express')
-const router = new Router()
+import { Router } from 'express'
+const router = Router()
 const RunQuery = require('../lib/connectionPool')
 
 router.post('/', async (req, res) => {
--- src/api/tables.ts
+++ src/api/tables.ts
@@ -1,5 +1,5 @@
-const { Router } = require('express')
-const router = new Router()
+import { Router } from 'express'
+const router = Router()
 const { tables } = require('../lib/sql')
 const RunQuery = require('../lib/connectionPool')
 import { DEFAULT_SYSTEM_SCHEMAS } from '../lib/constants/schemas'
