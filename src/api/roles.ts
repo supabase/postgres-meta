cd2ba944aabc5e4e64894874f1f611f143b74e66
Bobbie Soedirgo <bobbie@soedirgo.dev>
2020-06-23 23:15:54 +0800

    feat: create roles and tables from endpoints

--- src/api/roles.ts
+++ src/api/roles.ts
@@ -36,6 +36,43 @@ FROM
     res.status(500).json({ error: 'Database error', status: 500 })
   }
 })
+router.post('/', async (req, res) => {
+  try {
+    const {
+      name,
+      is_super_user = false,
+      has_create_db_privileges = false,
+      has_replication_privileges = false,
+      can_bypass_rls = false,
+      connections = -1,
+      valid_until,
+    } = req.body as {
+      name: string
+      is_super_user?: boolean
+      has_create_db_privileges?: boolean
+      has_replication_privileges?: boolean
+      can_bypass_rls?: boolean
+      connections?: number
+      valid_until?: string
+    }
+    const sql = `
+CREATE ROLE ${name}
+WITH
+  ${is_super_user ? 'SUPERUSER' : 'NOSUPERUSER'}
+  ${has_create_db_privileges ? 'CREATEDB' : 'NOCREATEDB'}
+  ${has_replication_privileges ? 'REPLICATION' : 'NOREPLICATION'}
+  ${can_bypass_rls ? 'BYPASSRLS' : 'NOBYPASSRLS'}
+  CONNECTION LIMIT ${connections}
+  ${valid_until === undefined ? '' : `VALID UNTIL '${valid_until}'`}`
+    console.log(sql)
+    const { data } = await RunQuery(req.headers.pg, sql)
+    return res.status(200).json(data)
+  } catch (error) {
+    // For this one, we always want to give back the error to the customer
+    console.log('Soft error!', error)
+    res.status(200).json([{ error: error.toString() }])
+  }
+})
 
 const removeSystemSchemas = (data: Roles.Role[]) => {
   return data.map((role) => {
