b79a4d4bcb2725b6aa9e1be7229f16c2a4699a1b
Paul Copplestone <pcopplestone@gmail.com>
2020-07-05 15:51:04 +0800

    feat: adds the ability to create and update a schema

--- src/api/schemas.ts
+++ src/api/schemas.ts
@@ -1,10 +1,11 @@
 import { Router } from 'express'
-
-import sql = require('../lib/sql')
-const { schemas } = sql
+import SQL from 'sql-template-strings'
+import sqlTemplates = require('../lib/sql')
 import { RunQuery } from '../lib/connectionPool'
 import { DEFAULT_SYSTEM_SCHEMAS } from '../lib/constants'
 import { Schemas } from '../lib/interfaces'
+
+const { schemas } = sqlTemplates
 
 /**
  * @param {boolean} [includeSystemSchemas=false] - Return system schemas as well as user schemas
@@ -14,6 +15,7 @@ interface GetSchemasQueryParams {
 }
 
 const router = Router()
+
 router.get('/', async (req, res) => {
   try {
     const { data } = await RunQuery(req.headers.pg, schemas)
@@ -23,11 +25,81 @@ router.get('/', async (req, res) => {
 
     return res.status(200).json(payload)
   } catch (error) {
-    console.log('throwing error')
+    console.log('throwing error', error)
+    res.status(500).json({ error: 'Database error', status: 500 })
+  }
+})
+router.post('/', async (req, res) => {
+  try {
+    const name: string = req.body.name
+    const owner: string = req.body.owner
+
+    // Create the schema
+    const schemqQuery = createSchema(name, owner)
+    await RunQuery(req.headers.pg, schemqQuery)
+  
+    // Return fresh details
+    const getSchema = selectSingleByName(name)
+    const { data } = await RunQuery(req.headers.pg, getSchema)
+    let schema: Schemas.Schema = data[0]
+    return res.status(200).json(schema)
+  } catch (error) {
+    console.log('throwing error', error)
+    res.status(500).json({ error: 'Database error', status: 500 })
+  }
+})
+router.patch('/:id', async (req, res) => {
+  try {
+    const id: number = parseInt(req.params.id)
+    const name: string = req.body.name
+    const owner: string = req.body.owner
+
+    // Get schema name
+    const getSchema = selectSingleSql(id)
+    const { data: getSchemaResults } = await RunQuery(req.headers.pg, getSchema)
+    let previousSchema: Schemas.Schema = getSchemaResults[0]
+
+    // Update fields
+    if (owner) {
+      const updateOwner = alterSchemaOwner(previousSchema.name, owner)
+      await RunQuery(req.headers.pg, updateOwner)
+    }
+    if (name) {
+      const updateName = alterSchemaName(previousSchema.name, name)
+      await RunQuery(req.headers.pg, updateName)
+    }
+
+    // Return fresh details
+    const { data: updatedSchemaResults } = await RunQuery(req.headers.pg, getSchema)
+    let updatedSchema: Schemas.Schema = updatedSchemaResults[0]
+    return res.status(200).json(updatedSchema)
+  } catch (error) {
+    console.log('throwing error', error)
     res.status(500).json({ error: 'Database error', status: 500 })
   }
 })
 
+// Helpers
+const selectSingleSql = (id: number) => {
+  const query = SQL``.append(schemas).append(SQL` where nsp.oid = ${id}`)
+  return query
+}
+const selectSingleByName = (name: string) => {
+  const query = SQL``.append(schemas).append(SQL` where schema_name = ${name}`)
+  return query
+}
+const createSchema = (name: string, owner: string = 'postgres') => {
+  const query = SQL``.append(`CREATE SCHEMA IF NOT EXISTS ${name} AUTHORIZATION ${owner}`)
+  return query
+}
+const alterSchemaName = (previousName: string, newName: string) => {
+  const query = SQL``.append(`ALTER SCHEMA ${previousName} RENAME TO ${newName}`)
+  return query
+}
+const alterSchemaOwner = (schemaName: string, newOwner: string) => {
+  const query = SQL``.append(`ALTER SCHEMA ${schemaName} OWNER TO ${newOwner}`)
+  return query
+}
 const removeSystemSchemas = (data: Schemas.Schema[]) => {
   return data.filter((x) => !DEFAULT_SYSTEM_SCHEMAS.includes(x.name))
 }
--- src/lib/connectionPool.ts
+++ src/lib/connectionPool.ts
@@ -1,11 +1,12 @@
 import pg = require('pg')
+import { SQLStatement } from 'sql-template-strings'
 // HACK: Number has 53 bits of precision, may overflow with bigint (64 bits).
 // Maybe use BigInt?
 // TODO: Use Date object for timestamptz?
 pg.types.setTypeParser(20, 'text', parseInt)
 const { Pool } = pg
 
-export const RunQuery = async (connectionString: any, sql: string) => {
+export const RunQuery = async (connectionString: any, sql: string|SQLStatement) => {
   const pool = new Pool({ connectionString })
   try {
     const results = await pool.query(sql)
