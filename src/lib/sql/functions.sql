d2fb9295c35558fcf3f14cb1a5b3a5e11f9aa218
Paul Copplestone <pcopplestone@gmail.com>
2020-07-06 15:02:03 +0800

    feat: adds /functions
    Returns all the database functions and stored procedures

---
+++ src/lib/sql/functions.sql
@@ -0,0 +1,16 @@
+select
+    p.oid as id,
+    n.nspname as schema,
+    p.proname as name,
+    l.lanname as language,
+    case
+        when l.lanname = 'internal' then p.prosrc
+        else pg_get_functiondef(p.oid)
+    end as definition,
+    pg_get_function_arguments(p.oid) as argument_types,
+    t.typname as return_type
+from
+    pg_proc p
+    left join pg_namespace n on p.pronamespace = n.oid
+    left join pg_language l on p.prolang = l.oid
+    left join pg_type t on t.oid = p.prorettype
