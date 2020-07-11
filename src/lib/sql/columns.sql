b9e49b9dcce4b7a38d2fbf8eef053d58612d663c
Paul Copplestone <pcopplestone@gmail.com>
2020-07-11 20:21:14 +0800

    fix: Adds the enums key to the column
    Fixes #33

--- src/lib/sql/columns.sql
+++ src/lib/sql/columns.sql
@@ -8,15 +8,24 @@ SELECT
   column_default AS default_value,
   data_type,
   udt_name AS format,
-  col_description(
-    c.oid,
-    ordinal_position
-  ) AS description,
-  is_identity::boolean,
+  col_description(c.oid, ordinal_position) AS description,
+  is_identity :: boolean,
   identity_generation,
-  is_nullable::boolean,
-  is_updatable::boolean
+  is_nullable :: boolean,
+  is_updatable :: boolean,
+  array_to_json(
+    array(
+      SELECT
+        enumlabel
+      FROM
+        pg_catalog.pg_enum enums
+      WHERE
+        udt_name = pg_catalog.Format_type(enums.enumtypid :: regclass, NULL)
+      ORDER BY
+        enums.enumsortorder
+    )
+  ) AS enums
 FROM
   information_schema.columns
-  JOIN pg_class c ON quote_ident(table_schema)::regnamespace = c.relnamespace
+  JOIN pg_class c ON quote_ident(table_schema) :: regnamespace = c.relnamespace
   AND c.relname = table_name
