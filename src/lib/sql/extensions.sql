05f53b40e67d6c2289ef8e5fbaabbf1cb463abb2
Bobbie Soedirgo <bobbie@soedirgo.dev>
2020-12-17 19:40:48 +0800

    feat: add schema to /extensions

--- src/lib/sql/extensions.sql
+++ src/lib/sql/extensions.sql
@@ -1,7 +1,9 @@
 SELECT
-  name,
-  comment,
-  default_version,
-  installed_version
+  e.name,
+  x.extnamespace :: regnamespace AS schema,
+  e.default_version,
+  x.extversion AS installed_version,
+  e.comment
 FROM
-  pg_available_extensions
+  pg_available_extensions() e(name, default_version, comment)
+  LEFT JOIN pg_extension x ON e.name = x.extname
