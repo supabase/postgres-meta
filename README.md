f5e7766779e23c4dd2bc3685b5b2897f6ffb590a
Bobbie Soedirgo <bobbie@soedirgo.dev>
2021-02-04 19:24:03 +0800

    chore: finalize pg-api -> postgres-meta
    Things renamed:
    * Repo name & url, docs, names in config files
    * Docker image name
    * Binary name (pg-api-<os> -> postgres-meta-<os>)
    * Envs (PG_API_* -> PG_META_*)

--- README.md
+++ README.md
@@ -11,12 +11,12 @@ Full documentation: https://supabase.github.io/postgres-meta/
 Set the following ENV VARS:
 
 ```bash
-PG_API_PORT=8080
-PG_API_DB_HOST="postgres"
-PG_API_DB_NAME="postgres"
-PG_API_DB_USER="postgres"
-PG_API_DB_PORT=5432
-PG_API_DB_PASSWORD="postgres"
+PG_META_PORT=8080
+PG_META_DB_HOST="postgres"
+PG_META_DB_NAME="postgres"
+PG_META_DB_USER="postgres"
+PG_META_DB_PORT=5432
+PG_META_DB_PASSWORD="postgres"
 ```
 
 Then run any of the binaries in the releases.
@@ -39,6 +39,6 @@ Apache 2.0
 
 ## Sponsors
 
-We are building the features of Firebase using enterprise-grade, open source products. We support existing communities wherever possible, and if the products don’t exist we build them and open source them ourselves. 
+We are building the features of Firebase using enterprise-grade, open source products. We support existing communities wherever possible, and if the products don’t exist we build them and open source them ourselves.
 
 [![New Sponsor](https://user-images.githubusercontent.com/10214025/90518111-e74bbb00-e198-11ea-8f88-c9e3c1aa4b5b.png)](https://github.com/sponsors/supabase)
--- src/server/constants.ts
+++ src/server/constants.ts
@@ -1,10 +1,10 @@
-export const PG_API_PORT = Number(process.env.PG_API_PORT || 1337)
+export const PG_META_PORT = Number(process.env.PG_META_PORT || 1337)
 export const CRYPTO_KEY = process.env.CRYPTO_KEY || 'SAMPLE_KEY'
 
-const PG_API_DB_HOST = process.env.PG_API_DB_HOST || 'localhost'
-const PG_API_DB_NAME = process.env.PG_API_DB_NAME || 'postgres'
-const PG_API_DB_USER = process.env.PG_API_DB_USER || 'postgres'
-const PG_API_DB_PORT = Number(process.env.PG_API_DB_PORT) || 5432
-const PG_API_DB_PASSWORD = process.env.PG_API_DB_PASSWORD || 'postgres'
+const PG_META_DB_HOST = process.env.PG_META_DB_HOST || 'localhost'
+const PG_META_DB_NAME = process.env.PG_META_DB_NAME || 'postgres'
+const PG_META_DB_USER = process.env.PG_META_DB_USER || 'postgres'
+const PG_META_DB_PORT = Number(process.env.PG_META_DB_PORT) || 5432
+const PG_META_DB_PASSWORD = process.env.PG_META_DB_PASSWORD || 'postgres'
 
-export const PG_CONNECTION = `postgres://${PG_API_DB_USER}:${PG_API_DB_PASSWORD}@${PG_API_DB_HOST}:${PG_API_DB_PORT}/${PG_API_DB_NAME}`
+export const PG_CONNECTION = `postgres://${PG_META_DB_USER}:${PG_META_DB_PASSWORD}@${PG_META_DB_HOST}:${PG_META_DB_PORT}/${PG_META_DB_NAME}`
