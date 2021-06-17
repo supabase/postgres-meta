0c8ff5ca9dd6ca6f4b4033719acfb30a2e3cfb2d
Paul Copplestone <pcopplestone@gmail.com>
2021-06-17 20:17:48 +0800

    Mocks up the triggers endpoint
    At this point we are returning tiggers but the rest of the CRUD is not working

---
+++ src/server/routes/triggers.ts
@@ -0,0 +1,114 @@
+import { FastifyInstance } from 'fastify'
+import { PostgresMeta } from '../../lib'
+
+export default async (fastify: FastifyInstance) => {
+  fastify.get<{
+    Headers: { pg: string }
+    Querystring: {
+      include_system_schemas?: string
+    }
+  }>('/', async (request, reply) => {
+    const connectionString = request.headers.pg
+    const includeSystemSchemas = request.query.include_system_schemas === 'true'
+
+    
+    const pgMeta = new PostgresMeta({ connectionString, max: 1 })
+    const { data, error } = await pgMeta.triggers.list({ includeSystemSchemas })
+    await pgMeta.end()
+    if (error) {
+      request.log.error(JSON.stringify({ error, req: request.body }))
+      reply.code(500)
+      return { error: error.message }
+    }
+
+    return data
+  })
+
+  fastify.get<{
+    Headers: { pg: string }
+    Params: {
+      id: string
+    }
+  }>('/:id(\\d+)', async (request, reply) => {
+    // const connectionString = request.headers.pg
+    // const id = Number(request.params.id)
+
+    // const pgMeta = new PostgresMeta({ connectionString, max: 1 })
+    // const { data, error } = await pgMeta.tables.retrieve({ id })
+    // await pgMeta.end()
+    // if (error) {
+    //   request.log.error({ error, req: request.body })
+    //   reply.code(404)
+    //   return { error: error.message }
+    // }
+
+    return { hello: 'world' }
+  })
+
+  fastify.post<{
+    Headers: { pg: string }
+    Body: any
+  }>('/', async (request, reply) => {
+    const connectionString = request.headers.pg
+
+    const pgMeta = new PostgresMeta({ connectionString, max: 1 })
+    const { data, error } = await pgMeta.tables.create(request.body)
+    await pgMeta.end()
+    if (error) {
+      request.log.error(JSON.stringify({ error, req: request.body }))
+      reply.code(400)
+      return { error: error.message }
+    }
+
+    return data
+  })
+
+  fastify.patch<{
+    Headers: { pg: string }
+    Params: {
+      id: string
+    }
+    Body: any
+  }>('/:id(\\d+)', async (request, reply) => {
+    const connectionString = request.headers.pg
+    const id = Number(request.params.id)
+
+    const pgMeta = new PostgresMeta({ connectionString, max: 1 })
+    const { data, error } = await pgMeta.tables.update(id, request.body)
+    await pgMeta.end()
+    if (error) {
+      request.log.error(JSON.stringify({ error, req: request.body }))
+      reply.code(400)
+      if (error.message.startsWith('Cannot find')) reply.code(404)
+      return { error: error.message }
+    }
+
+    return data
+  })
+
+  fastify.delete<{
+    Headers: { pg: string }
+    Params: {
+      id: string
+    }
+    Querystring: {
+      cascade?: string
+    }
+  }>('/:id(\\d+)', async (request, reply) => {
+    const connectionString = request.headers.pg
+    const id = Number(request.params.id)
+    const cascade = request.query.cascade === 'true'
+
+    const pgMeta = new PostgresMeta({ connectionString, max: 1 })
+    const { data, error } = await pgMeta.tables.remove(id, { cascade })
+    await pgMeta.end()
+    if (error) {
+      request.log.error(JSON.stringify({ error, req: request.body }))
+      reply.code(400)
+      if (error.message.startsWith('Cannot find')) reply.code(404)
+      return { error: error.message }
+    }
+
+    return data
+  })
+}
