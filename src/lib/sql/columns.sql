97a1a7a23706c343133e6867c0531958c423cca7
Bobbie Soedirgo <bobbie@soedirgo.dev>
2021-06-21 11:23:10 +0800

    fix(lib): missing enum values when typname quoted

--- src/lib/sql/columns.sql
+++ src/lib/sql/columns.sql
@@ -59,7 +59,7 @@ SELECT
       FROM
         pg_catalog.pg_enum enums
       WHERE
-        COALESCE(bt.typname, t.typname) = format_type(enums.enumtypid, NULL)
+        quote_ident(COALESCE(bt.typname, t.typname)) = format_type(enums.enumtypid, NULL)
       ORDER BY
         enums.enumsortorder
     )
