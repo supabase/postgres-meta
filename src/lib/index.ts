9ca63bc6d5a2d291a9189dda8b5062efe034b019
Strift <lau.cazanove@gmail.com>
2021-07-18 16:45:18 +0200

    feat: Export types (#130)
    * Export types defined in types.ts
    
    * Export only types

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,2 +1,20 @@
 import PostgresMeta from './PostgresMeta'
 export { PostgresMeta }
+export {
+  PostgresMetaResult,
+  PostgresColumn,
+  PostgresConfig,
+  PostgresExtension,
+  PostgresFunction,
+  PostgresGrant,
+  PostgresPolicy,
+  PostgresPrimaryKey,
+  PostgresPublication,
+  PostgresRelationship,
+  PostgresRole,
+  PostgresSchema,
+  PostgresSchemaCreate,
+  PostgresSchemaUpdate,
+  PostgresTable,
+  PostgresType
+} from './types'
