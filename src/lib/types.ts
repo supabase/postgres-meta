3d9b5ba5b05284cc00f53dd9b6a27a3a20ef9be1
Copple <10214025+kiwicopple@users.noreply.github.com>
2021-11-21 14:03:58 +0800

    prettier

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -2,7 +2,6 @@ import { Static, Type } from '@sinclair/typebox'
 import { Options as PrettierOptions } from 'prettier'
 import { Option as SqlParserOptions, TableColumnAst } from 'node-sql-parser/build/postgresql'
 
-
 export interface ParserOptions extends PrettierOptions {}
 export interface ParsedAst extends TableColumnAst {}
 export interface FormatterOptions extends SqlParserOptions {}
