edbac8d8d1b8e5d59f4efa34ab1353ba84d759aa
Bobbie Soedirgo <bobbie@soedirgo.dev>
2022-06-24 00:22:46 +0800

    feat: shoehorn CLI-mode
    Used for docs export & typegen.
    
    Probably need to move it to `src/cli` in the future, but works well
    enough for now.

--- src/server/constants.ts
+++ src/server/constants.ts
@@ -12,7 +12,11 @@ const PG_CONN_TIMEOUT_SECS = Number(process.env.PG_CONN_TIMEOUT_SECS || 15)
 
 export const PG_CONNECTION = `postgres://${PG_META_DB_USER}:${PG_META_DB_PASSWORD}@${PG_META_DB_HOST}:${PG_META_DB_PORT}/${PG_META_DB_NAME}?sslmode=${PG_META_DB_SSL_MODE}`
 
-export const PG_META_EXPORT_DOCS = process.env.PG_META_EXPORT_DOCS === 'true' || false
+export const EXPORT_DOCS = process.argv[2] === 'docs' && process.argv[3] === 'export'
+export const GENERATE_TYPES =
+  process.argv[2] === 'gen' && process.argv[3] === 'types' ? process.argv[4] : undefined
+export const GENERATE_TYPES_EXCLUDED_SCHEMAS =
+  GENERATE_TYPES && process.argv[5] === '--exclude-schemas' ? process.argv[6]?.split(',') ?? [] : []
 
 export const DEFAULT_POOL_CONFIG = { max: 1, connectionTimeoutMillis: PG_CONN_TIMEOUT_SECS * 1000 }
 export const PG_META_REQ_HEADER = process.env.PG_META_REQ_HEADER || 'request-id'
