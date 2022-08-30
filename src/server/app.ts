234f1bc98d5013e31131b0cbdd75ff5752f009d5
Bobbie Soedirgo <bobbie@soedirgo.dev>
2022-08-30 13:08:46 +0800

    feat(typegen): views

--- src/server/app.ts
+++ src/server/app.ts
@@ -65,6 +65,7 @@ if (EXPORT_DOCS) {
     })
     const { data: schemas, error: schemasError } = await pgMeta.schemas.list()
     const { data: tables, error: tablesError } = await pgMeta.tables.list()
+    const { data: views, error: viewsError } = await pgMeta.views.list()
     const { data: functions, error: functionsError } = await pgMeta.functions.list()
     const { data: types, error: typesError } = await pgMeta.types.list({
       includeSystemSchemas: true,
@@ -76,6 +77,9 @@ if (EXPORT_DOCS) {
     }
     if (tablesError) {
       throw new Error(tablesError.message)
+    }
+    if (viewsError) {
+      throw new Error(viewsError.message)
     }
     if (functionsError) {
       throw new Error(functionsError.message)
@@ -92,6 +96,7 @@ if (EXPORT_DOCS) {
             GENERATE_TYPES_INCLUDED_SCHEMAS.includes(name)
         ),
         tables,
+        views,
         functions: functions.filter(
           ({ return_type }) => !['trigger', 'event_trigger'].includes(return_type)
         ),
