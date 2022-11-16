578cd343f5d0bd85cbd5bbc0187ca999dd977102
Bobbie Soedirgo <bobbie@soedirgo.dev>
2022-11-16 18:40:30 +0800

    fix: type errors

--- src/server/routes/tables.ts
+++ src/server/routes/tables.ts
@@ -57,7 +57,7 @@ export default async (fastify: FastifyInstance) => {
     const connectionString = request.headers.pg
 
     const pgMeta = new PostgresMeta({ ...DEFAULT_POOL_CONFIG, connectionString })
-    const { data, error } = await pgMeta.tables.create(request.body)
+    const { data, error } = await pgMeta.tables.create(request.body as any)
     await pgMeta.end()
     if (error) {
       request.log.error({ error, request: extractRequestForLogging(request) })
@@ -79,7 +79,7 @@ export default async (fastify: FastifyInstance) => {
     const id = Number(request.params.id)
 
     const pgMeta = new PostgresMeta({ ...DEFAULT_POOL_CONFIG, connectionString })
-    const { data, error } = await pgMeta.tables.update(id, request.body)
+    const { data, error } = await pgMeta.tables.update(id, request.body as any)
     await pgMeta.end()
     if (error) {
       request.log.error({ error, request: extractRequestForLogging(request) })
