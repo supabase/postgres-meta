febf50b5dea905305ea4857e9b3aadafa1bd9056
Bobbie Soedirgo <bobbie@soedirgo.dev>
2022-11-18 17:57:13 +0800

    fix: bump fastify-metrics

--- src/server/admin-app.ts
+++ src/server/admin-app.ts
@@ -1,13 +1,12 @@
 import fastify, { FastifyInstance, FastifyServerOptions } from 'fastify'
 
-import _default = require('fastify-metrics')
+import FastifyMetrics from 'fastify-metrics'
 
 export function build(opts: FastifyServerOptions = {}): FastifyInstance {
   const app = fastify(opts)
-  app.register(_default, {
+  app.register(FastifyMetrics, {
     endpoint: '/metrics',
-    enableRouteMetrics: false,
-    blacklist: ['nodejs_version_info', 'process_start_time_seconds'],
+    routeMetrics: { enabled: false },
   })
   return app
 }
