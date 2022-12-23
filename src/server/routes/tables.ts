4930d43f9142a8699fba0ef5c2655aa85fb58099
Qiao Han <qiao@supabase.io>
2022-12-23 22:35:41 +0800

    chore: add query param to route

--- src/server/routes/tables.ts
+++ src/server/routes/tables.ts
@@ -8,13 +8,22 @@ export default async (fastify: FastifyInstance) => {
     Headers: { pg: string }
     Querystring: {
       include_system_schemas?: string
+      // Note: this only supports comma separated values (e.g., ".../tables?included_schemas=public,core")
+      included_schemas?: string
+      excluded_schemas?: string
     }
   }>('/', async (request, reply) => {
     const connectionString = request.headers.pg
     const includeSystemSchemas = request.query.include_system_schemas === 'true'
+    const includedSchemas = request.query.included_schemas?.split(',')
+    const excludedSchemas = request.query.excluded_schemas?.split(',')
 
     const pgMeta = new PostgresMeta({ ...DEFAULT_POOL_CONFIG, connectionString })
-    const { data, error } = await pgMeta.tables.count({ includeSystemSchemas })
+    const { data, error } = await pgMeta.tables.count({
+      includeSystemSchemas,
+      includedSchemas,
+      excludedSchemas,
+    })
     await pgMeta.end()
     if (error) {
       request.log.error({ error, request: extractRequestForLogging(request) })
