d47d42b1793c28298333e8e2eaa90f573788947f
Bobbie Soedirgo <bobbie@soedirgo.dev>
2023-01-20 14:58:23 +0800

    test(views, foreign-tables): retrieve

--- test/lib/views.ts
+++ test/lib/views.ts
@@ -92,3 +92,81 @@ test('list without columns', async () => {
   `
   )
 })
+
+test('retrieve', async () => {
+  const res = await pgMeta.views.retrieve({ schema: 'public', name: 'todos_view' })
+  expect(res).toMatchInlineSnapshot(
+    { data: { id: expect.any(Number) } },
+    `
+    {
+      "data": {
+        "columns": [
+          {
+            "comment": null,
+            "data_type": "bigint",
+            "default_value": null,
+            "enums": [],
+            "format": "int8",
+            "id": "16420.1",
+            "identity_generation": null,
+            "is_generated": false,
+            "is_identity": false,
+            "is_nullable": true,
+            "is_unique": false,
+            "is_updatable": true,
+            "name": "id",
+            "ordinal_position": 1,
+            "schema": "public",
+            "table": "todos_view",
+            "table_id": 16420,
+          },
+          {
+            "comment": null,
+            "data_type": "text",
+            "default_value": null,
+            "enums": [],
+            "format": "text",
+            "id": "16420.2",
+            "identity_generation": null,
+            "is_generated": false,
+            "is_identity": false,
+            "is_nullable": true,
+            "is_unique": false,
+            "is_updatable": true,
+            "name": "details",
+            "ordinal_position": 2,
+            "schema": "public",
+            "table": "todos_view",
+            "table_id": 16420,
+          },
+          {
+            "comment": null,
+            "data_type": "bigint",
+            "default_value": null,
+            "enums": [],
+            "format": "int8",
+            "id": "16420.3",
+            "identity_generation": null,
+            "is_generated": false,
+            "is_identity": false,
+            "is_nullable": true,
+            "is_unique": false,
+            "is_updatable": true,
+            "name": "user-id",
+            "ordinal_position": 3,
+            "schema": "public",
+            "table": "todos_view",
+            "table_id": 16420,
+          },
+        ],
+        "comment": null,
+        "id": Any<Number>,
+        "is_updatable": true,
+        "name": "todos_view",
+        "schema": "public",
+      },
+      "error": null,
+    }
+  `
+  )
+})
