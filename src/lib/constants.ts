60d5d70657e76d63986a1fc0996540dd351b3fca
Ruggero Tomaselli <ruggerotomaselli@gmail.com>
2023-01-30 09:09:43 +0100

    fix: logic when returning default roles (#490)
    * Bug fix returning default roles
    
    * refactor: replace DEFAULT_ROLES w/ prefix test
    
    * test: includeDefaultRoles
    
    ---------
    
    Co-authored-by: Bobbie Soedirgo <bobbie@soedirgo.dev>

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,14 +1,3 @@
-export const DEFAULT_ROLES = [
-  'pg_execute_server_program',
-  'pg_monitor',
-  'pg_read_all_settings',
-  'pg_read_all_stats',
-  'pg_read_server_files',
-  'pg_signal_backend',
-  'pg_stat_scan_tables',
-  'pg_write_server_files',
-]
-
 export const DEFAULT_SYSTEM_SCHEMAS = [
   'information_schema',
   'pg_catalog',
