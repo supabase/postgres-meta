8edb057bbba1eb76c6b7b782d97765cf1701ab16
Bobbie Soedirgo <bobbie@soedirgo.dev>
2023-02-01 22:50:36 +0800

    fix: always ignore temporary schemas
    i.e. `pg_temp_*`, `pg_toast_temp_*`

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,7 +1,1 @@
-export const DEFAULT_SYSTEM_SCHEMAS = [
-  'information_schema',
-  'pg_catalog',
-  'pg_temp_1',
-  'pg_toast',
-  'pg_toast_temp_1',
-]
+export const DEFAULT_SYSTEM_SCHEMAS = ['information_schema', 'pg_catalog', 'pg_toast']
