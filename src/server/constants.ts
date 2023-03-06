598f7a7a144e042764b27586761d142d79673db7
Bobbie Soedirgo <bobbie@soedirgo.dev>
2023-03-06 14:20:38 +0800

    chore: remove unused constant

--- src/server/constants.ts
+++ src/server/constants.ts
@@ -13,7 +13,6 @@ const PG_META_DB_SSL_MODE = process.env.PG_META_DB_SSL_MODE || 'disable'
 
 const PG_CONN_TIMEOUT_SECS = Number(process.env.PG_CONN_TIMEOUT_SECS || 15)
 
-export const PG_META_EXPORT_DOCS = process.env.PG_META_EXPORT_DOCS === 'true' || false
 export const PG_CONNECTION =
   process.env.PG_META_DB_URL ||
   `postgres://${PG_META_DB_USER}:${encodeURIComponent(
