ea6be2c1cef3aad631b5680ac659674dc979e4a7
Bobbie Soedirgo <bobbie@soedirgo.dev>
2023-11-02 14:44:37 +0800

    feat(typegen): make 1-to-1 relationships detection optional

--- test/server/typegen.ts
+++ test/server/typegen.ts
@@ -61,6 +61,359 @@ test('typegen', async () => {
               {
                 foreignKeyName: "memes_category_fkey"
                 columns: ["category"]
+                referencedRelation: "category"
+                referencedColumns: ["id"]
+              }
+            ]
+          }
+          todos: {
+            Row: {
+              details: string | null
+              id: number
+              "user-id": number
+              blurb: string | null
+            }
+            Insert: {
+              details?: string | null
+              id?: number
+              "user-id": number
+            }
+            Update: {
+              details?: string | null
+              id?: number
+              "user-id"?: number
+            }
+            Relationships: [
+              {
+                foreignKeyName: "todos_user-id_fkey"
+                columns: ["user-id"]
+                referencedRelation: "a_view"
+                referencedColumns: ["id"]
+              },
+              {
+                foreignKeyName: "todos_user-id_fkey"
+                columns: ["user-id"]
+                referencedRelation: "users"
+                referencedColumns: ["id"]
+              },
+              {
+                foreignKeyName: "todos_user-id_fkey"
+                columns: ["user-id"]
+                referencedRelation: "users_view"
+                referencedColumns: ["id"]
+              }
+            ]
+          }
+          user_details: {
+            Row: {
+              details: string | null
+              user_id: number
+            }
+            Insert: {
+              details?: string | null
+              user_id: number
+            }
+            Update: {
+              details?: string | null
+              user_id?: number
+            }
+            Relationships: [
+              {
+                foreignKeyName: "user_details_user_id_fkey"
+                columns: ["user_id"]
+                referencedRelation: "a_view"
+                referencedColumns: ["id"]
+              },
+              {
+                foreignKeyName: "user_details_user_id_fkey"
+                columns: ["user_id"]
+                referencedRelation: "users"
+                referencedColumns: ["id"]
+              },
+              {
+                foreignKeyName: "user_details_user_id_fkey"
+                columns: ["user_id"]
+                referencedRelation: "users_view"
+                referencedColumns: ["id"]
+              }
+            ]
+          }
+          users: {
+            Row: {
+              id: number
+              name: string | null
+              status: Database["public"]["Enums"]["user_status"] | null
+            }
+            Insert: {
+              id?: number
+              name?: string | null
+              status?: Database["public"]["Enums"]["user_status"] | null
+            }
+            Update: {
+              id?: number
+              name?: string | null
+              status?: Database["public"]["Enums"]["user_status"] | null
+            }
+            Relationships: []
+          }
+          users_audit: {
+            Row: {
+              created_at: string | null
+              id: number
+              previous_value: Json | null
+              user_id: number | null
+            }
+            Insert: {
+              created_at?: string | null
+              id?: number
+              previous_value?: Json | null
+              user_id?: number | null
+            }
+            Update: {
+              created_at?: string | null
+              id?: number
+              previous_value?: Json | null
+              user_id?: number | null
+            }
+            Relationships: []
+          }
+        }
+        Views: {
+          a_view: {
+            Row: {
+              id: number | null
+            }
+            Insert: {
+              id?: number | null
+            }
+            Update: {
+              id?: number | null
+            }
+            Relationships: []
+          }
+          todos_matview: {
+            Row: {
+              details: string | null
+              id: number | null
+              "user-id": number | null
+            }
+            Relationships: [
+              {
+                foreignKeyName: "todos_user-id_fkey"
+                columns: ["user-id"]
+                referencedRelation: "users"
+                referencedColumns: ["id"]
+              },
+              {
+                foreignKeyName: "todos_user-id_fkey"
+                columns: ["user-id"]
+                referencedRelation: "a_view"
+                referencedColumns: ["id"]
+              },
+              {
+                foreignKeyName: "todos_user-id_fkey"
+                columns: ["user-id"]
+                referencedRelation: "users_view"
+                referencedColumns: ["id"]
+              }
+            ]
+          }
+          todos_view: {
+            Row: {
+              details: string | null
+              id: number | null
+              "user-id": number | null
+            }
+            Insert: {
+              details?: string | null
+              id?: number | null
+              "user-id"?: number | null
+            }
+            Update: {
+              details?: string | null
+              id?: number | null
+              "user-id"?: number | null
+            }
+            Relationships: [
+              {
+                foreignKeyName: "todos_user-id_fkey"
+                columns: ["user-id"]
+                referencedRelation: "users"
+                referencedColumns: ["id"]
+              },
+              {
+                foreignKeyName: "todos_user-id_fkey"
+                columns: ["user-id"]
+                referencedRelation: "a_view"
+                referencedColumns: ["id"]
+              },
+              {
+                foreignKeyName: "todos_user-id_fkey"
+                columns: ["user-id"]
+                referencedRelation: "users_view"
+                referencedColumns: ["id"]
+              }
+            ]
+          }
+          users_view: {
+            Row: {
+              id: number | null
+              name: string | null
+              status: Database["public"]["Enums"]["user_status"] | null
+            }
+            Insert: {
+              id?: number | null
+              name?: string | null
+              status?: Database["public"]["Enums"]["user_status"] | null
+            }
+            Update: {
+              id?: number | null
+              name?: string | null
+              status?: Database["public"]["Enums"]["user_status"] | null
+            }
+            Relationships: []
+          }
+        }
+        Functions: {
+          blurb: {
+            Args: {
+              "": unknown
+            }
+            Returns: string
+          }
+          function_returning_row: {
+            Args: Record<PropertyKey, never>
+            Returns: {
+              id: number
+              name: string | null
+              status: Database["public"]["Enums"]["user_status"] | null
+            }
+          }
+          function_returning_set_of_rows: {
+            Args: Record<PropertyKey, never>
+            Returns: {
+              id: number
+              name: string | null
+              status: Database["public"]["Enums"]["user_status"] | null
+            }[]
+          }
+          function_returning_table: {
+            Args: Record<PropertyKey, never>
+            Returns: {
+              id: number
+              name: string
+            }[]
+          }
+          polymorphic_function:
+            | {
+                Args: {
+                  "": boolean
+                }
+                Returns: undefined
+              }
+            | {
+                Args: {
+                  "": string
+                }
+                Returns: undefined
+              }
+          postgres_fdw_disconnect: {
+            Args: {
+              "": string
+            }
+            Returns: boolean
+          }
+          postgres_fdw_disconnect_all: {
+            Args: Record<PropertyKey, never>
+            Returns: boolean
+          }
+          postgres_fdw_get_connections: {
+            Args: Record<PropertyKey, never>
+            Returns: Record<string, unknown>[]
+          }
+          postgres_fdw_handler: {
+            Args: Record<PropertyKey, never>
+            Returns: unknown
+          }
+        }
+        Enums: {
+          meme_status: "new" | "old" | "retired"
+          user_status: "ACTIVE" | "INACTIVE"
+        }
+        CompositeTypes: {
+          [_ in never]: never
+        }
+      }
+    }
+    "
+  `)
+})
+
+test('typegen w/ one-to-one relationships', async () => {
+  const { body } = await app.inject({
+    method: 'GET',
+    path: '/generators/typescript',
+    query: { detect_one_to_one_relationships: 'true' },
+  })
+  // This is expected to be very brittle, as generated types can change a lot
+  // without being breaking changes. Use `npm run test:update` to make life
+  // easier.
+  expect(body).toMatchInlineSnapshot(`
+    "export type Json =
+      | string
+      | number
+      | boolean
+      | null
+      | { [key: string]: Json | undefined }
+      | Json[]
+
+    export interface Database {
+      public: {
+        Tables: {
+          category: {
+            Row: {
+              id: number
+              name: string
+            }
+            Insert: {
+              id?: number
+              name: string
+            }
+            Update: {
+              id?: number
+              name?: string
+            }
+            Relationships: []
+          }
+          memes: {
+            Row: {
+              category: number | null
+              created_at: string
+              id: number
+              metadata: Json | null
+              name: string
+              status: Database["public"]["Enums"]["meme_status"] | null
+            }
+            Insert: {
+              category?: number | null
+              created_at: string
+              id?: number
+              metadata?: Json | null
+              name: string
+              status?: Database["public"]["Enums"]["meme_status"] | null
+            }
+            Update: {
+              category?: number | null
+              created_at?: string
+              id?: number
+              metadata?: Json | null
+              name?: string
+              status?: Database["public"]["Enums"]["meme_status"] | null
+            }
+            Relationships: [
+              {
+                foreignKeyName: "memes_category_fkey"
+                columns: ["category"]
                 isOneToOne: false
                 referencedRelation: "category"
                 referencedColumns: ["id"]
