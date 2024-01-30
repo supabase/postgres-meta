ea6d1c8aed033fb76786d7d3026d6b12ee9e889b
Copple <10214025+kiwicopple@users.noreply.github.com>
2024-01-30 07:04:37 +0100

    feat: adds route for indexes (#700)

---
+++ src/server/routes/indexes.ts
@@ -0,0 +1,63 @@
+import { FastifyInstance } from 'fastify'
+import { PostgresMeta } from '../../lib/index.js'
+import { DEFAULT_POOL_CONFIG } from '../constants.js'
+import { extractRequestForLogging } from '../utils.js'
+
+export default async (fastify: FastifyInstance) => {
+  fastify.get<{
+    Headers: { pg: string }
+    Querystring: {
+      include_system_schemas?: string
+      // Note: this only supports comma separated values (e.g., ".../functions?included_schemas=public,core")
+      included_schemas?: string
+      excluded_schemas?: string
+      limit?: number
+      offset?: number
+    }
+  }>('/', async (request, reply) => {
+    const connectionString = request.headers.pg
+    const includeSystemSchemas = request.query.include_system_schemas === 'true'
+    const includedSchemas = request.query.included_schemas?.split(',')
+    const excludedSchemas = request.query.excluded_schemas?.split(',')
+    const limit = request.query.limit
+    const offset = request.query.offset
+
+    const pgMeta = new PostgresMeta({ ...DEFAULT_POOL_CONFIG, connectionString })
+    const { data, error } = await pgMeta.indexes.list({
+      includeSystemSchemas,
+      includedSchemas,
+      excludedSchemas,
+      limit,
+      offset,
+    })
+    await pgMeta.end()
+    if (error) {
+      request.log.error({ error, request: extractRequestForLogging(request) })
+      reply.code(500)
+      return { error: error.message }
+    }
+
+    return data
+  })
+
+  fastify.get<{
+    Headers: { pg: string }
+    Params: {
+      id: string
+    }
+  }>('/:id(\\d+)', async (request, reply) => {
+    const connectionString = request.headers.pg
+    const id = Number(request.params.id)
+
+    const pgMeta = new PostgresMeta({ ...DEFAULT_POOL_CONFIG, connectionString })
+    const { data, error } = await pgMeta.indexes.retrieve({ id })
+    await pgMeta.end()
+    if (error) {
+      request.log.error({ error, request: extractRequestForLogging(request) })
+      reply.code(404)
+      return { error: error.message }
+    }
+
+    return data
+  })
+}
