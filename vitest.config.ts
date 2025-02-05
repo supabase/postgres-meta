8ad7e247b761bfe2dd7db75f6558af01831a58db
Bobbie Soedirgo <bobbie@soedirgo.dev>
2025-02-05 10:17:04 +0800

    chore: upload coverage to coveralls

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,6 +3,9 @@ import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
   test: {
+    coverage: {
+      reporter: ['lcov'],
+    },
     maxConcurrency: 1,
     // https://github.com/vitest-dev/vitest/issues/317#issuecomment-1542319622
     pool: 'forks',
