6d34fb357fceb0b22f3333b8c8cf8c1ea36f5ba5
avallete <andrew.valleteau@supabase.io>
2025-04-14 20:03:44 +0200

    fix: sentry rate events

--- src/server/sentry.ts
+++ src/server/sentry.ts
@@ -8,13 +8,13 @@ const captureOptions: Sentry.NodeOptions =
   sentryEnvironment === 'prod'
     ? {
         // Tracing
-        tracesSampleRate: 0.0001,
+        tracesSampleRate: 0.00001, // trace 1/10k events
         // Set sampling rate for profiling - this is evaluated only once per SDK.init call
-        profilesSampleRate: 0.0001,
+        profilesSampleRate: 0.00001, // profile 1/10k events
       }
     : {
-        tracesSampleRate: 1.0,
-        profilesSampleRate: 1.0,
+        tracesSampleRate: 0.01, // trace 1% of the events
+        profilesSampleRate: 0.01,
       }
 
 const sensitiveKeys = ['pg', 'x-connection-encrypted']
