55592859693ca7e2afea41e59c87b8c6602b9415
Leonardo Santiago <leonardo.ribeiro.santiago@gmail.com>
2025-11-17 11:52:21 -0300

    fix: add x-application-name header, add python to server.ts

--- src/server/routes/generators/python.ts
+++ src/server/routes/generators/python.ts
@@ -1,25 +1,23 @@
 import type { FastifyInstance } from 'fastify'
 import { PostgresMeta } from '../../../lib/index.js'
-import { DEFAULT_POOL_CONFIG } from '../../constants.js'
-import { extractRequestForLogging } from '../../utils.js'
+import { createConnectionConfig, extractRequestForLogging } from '../../utils.js'
 import { apply as applyPyTemplate } from '../../templates/python.js'
 import { getGeneratorMetadata } from '../../../lib/generators.js'
 
 export default async (fastify: FastifyInstance) => {
   fastify.get<{
-    Headers: { pg: string }
+    Headers: { pg: string; 'x-pg-application-name'?: string }
     Querystring: {
       excluded_schemas?: string
       included_schemas?: string
     }
   }>('/', async (request, reply) => {
-    const connectionString = request.headers.pg
+    const config = createConnectionConfig(request)
     const excludedSchemas =
       request.query.excluded_schemas?.split(',').map((schema) => schema.trim()) ?? []
     const includedSchemas =
       request.query.included_schemas?.split(',').map((schema) => schema.trim()) ?? []
-
-    const pgMeta: PostgresMeta = new PostgresMeta({ ...DEFAULT_POOL_CONFIG, connectionString })
+    const pgMeta: PostgresMeta = new PostgresMeta(config)
     const { data: generatorMeta, error: generatorMetaError } = await getGeneratorMetadata(pgMeta, {
       includedSchemas,
       excludedSchemas,
